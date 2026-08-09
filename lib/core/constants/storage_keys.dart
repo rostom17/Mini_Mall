1166b8ea5b74356f7313e9daec5bb71d9b11fb70
rostom17 <rostom825@gmail.com>
2026-08-09 17:50:35 +0600

    feat: add StorageKeys class for centralized storage key management

---
+++ lib/core/constants/storage_keys.dart
@@ -0,0 +1,8 @@
+abstract class StorageKeys {
+  const StorageKeys._();
+
+  // Storage Keys
+  static const String accessTokenKey = "__app_access_token__";
+  static const String refreshTokenKey = "__app_refresh_token__";
+  static const String themeModeKey = "__app_theme_mode__";
+}
