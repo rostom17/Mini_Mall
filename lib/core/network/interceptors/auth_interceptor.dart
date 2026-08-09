bdbb39bcf6208066eb15081e772f678e57538477
rostom17 <rostom825@gmail.com>
2026-08-09 19:49:08 +0600

    feat: add AuthInterceptor to attatch tokens

---
+++ lib/core/network/interceptors/auth_interceptor.dart
@@ -0,0 +1,60 @@
+import 'package:dio/dio.dart';
+import 'package:logger/logger.dart';
+import 'package:mini_mall/core/errors/exception.dart';
+
+import '/core/storage/token_storage.dart';
+import '/core/constants/api_constants.dart';
+
+class AuthInterceptor extends Interceptor {
+  const AuthInterceptor({required this._tokenService, required this._logger});
+
+  final TokenStorage _tokenService;
+  final Logger _logger;
+
+  @override
+  Future<void> onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    final token = await _tokenService.getAccessToken();
+    if (token == null) {
+      _logger.d("Token is null");
+    }
+    if (token != null) {
+      options.headers[ApiConstants.authorizationHeader] =
+          "${ApiConstants.bearer} $token";
+      handler.next(options);
+    }
+  }
+
+  @override
+  void onResponse(
+    Response<dynamic> response,
+    ResponseInterceptorHandler handler,
+  ) => handler.next(response);
+
+  @override
+  Future<void> onError(
+    DioException err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    _logger.e(
+      "Auth Interceptor (onError): ",
+      error: err.error,
+      stackTrace: err.stackTrace,
+    );
+    if (err.response?.statusCode == 401) {
+      _logger.e(err.message);
+      handler.reject(
+        DioException(
+          requestOptions: err.requestOptions,
+          error: const UnauthorizedException(),
+          response: err.response,
+          message: err.message,
+        ),
+      );
+      return;
+    }
+    handler.next(err);
+  }
+}
