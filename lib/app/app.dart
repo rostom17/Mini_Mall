7d1263c44a60eb2d4932a0bd11c939b760af58f8
rostom17 <rostom825@gmail.com>
2026-08-10 02:49:14 +0600

    feat: create root widget as the main application entry point

---
+++ lib/app/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MiniMall extends StatelessWidget {
+  const MiniMall({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(body: Center(child: Text("Home Page"))),
+    );
+  }
+}
